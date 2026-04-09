61c4c4a5fcf9dd59b3d6b7bb567d7ec89593f714
Wardi <wardi@nusa.net.id>
2026-04-09 12:01:14 +0700

    feat: setup BiomeJS for formatting and linting (#3)

--- src/domains/notification/jobs/send-welcome-email.job.ts
+++ src/domains/notification/jobs/send-welcome-email.job.ts
@@ -1,7 +1,7 @@
+import type { JsMsg } from 'nats';
 import { z } from 'zod';
 import { BaseJob } from '../../../core/base-job';
 import { logger } from '../../../core/logger';
-import { JsMsg } from 'nats';
 
 const PayloadSchema = z.object({
   userId: z.string(),
@@ -18,9 +18,9 @@ export class SendWelcomeEmailJob extends BaseJob<Payload> {
     return PayloadSchema.parse(data);
   }
 
-  protected async handle(payload: Payload, msg: JsMsg): Promise<void> {
+  protected async handle(payload: Payload, _msg: JsMsg): Promise<void> {
     logger.info(`Mengirim email ke ${payload.email} (User: ${payload.name})...`);
-    
+
     // Simulasi proses async (misal memanggil API SendGrid / SMTP)
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
